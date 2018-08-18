b849e52c7ec635705cb7911194b985995b845e2b
chiayen <38468171+yen2451@users.noreply.github.com>
2018-08-18 11:06:31 +0800

    update1

--- fileReadAndWrite.py.py
+++ fileReadAndWrite.py.py
@@ -2,3 +2,7 @@
 # fileReadAndWrite.py.py
 # @Author : chiayen hsu (yen_hsu's gmail:yen020224@gmail.com)
 # @Date   : 8/18/2018, 10:25:44 AM
+f1=open("text.txt",'r',1,encoding='utf8')
+tex=f1.readlines()
+f1.close()
+print(tex)
