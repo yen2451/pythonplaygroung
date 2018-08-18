18a94a8c90cf6fe5b812772adaa6512b3c3234fc
chiayen <38468171+yen2451@users.noreply.github.com>
2018-08-18 10:24:10 +0800

    pyt3074_not finished

--- pyt3074.py
+++ pyt3074.py
@@ -2,3 +2,12 @@
 # pyt3074.py
 # @Author : chiayen hsu (yen_hsu's gmail:yen020224@gmail.com)
 # @Date   : 8/18/2018, 9:56:44 AM
+import os, shutil
+if os.path.exists('files'):
+    os.rmdir('files')
+os.makedirs('files')
+os.chdir('files')
+i=int(input())
+for x in range(1,i+1):
+    x=int(x)
+    ##^^i don't know
